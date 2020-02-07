633b30fc9b2cd536d14525295929f057983c1511
avishka umayanga <35651169+umayanga1997@users.noreply.github.com>
2020-02-08 00:40:41 +0530

    Add files via upload

---
+++ Password Genarator/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        If TextBox1.Text = "" Then
+            MsgBox("Please Try Again....!")
+        Else
+            Label1.Text = (Val(TextBox1.Text) + Val(999999) + Val(55428) + Val(77810) / Val(2) * Val(45))
+            MsgBox("Your Password Is " & Label1.Text)
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TextBox1.Text = Focus()
+        TextBox1.Text = ""
+    End Sub
+
+End Class
